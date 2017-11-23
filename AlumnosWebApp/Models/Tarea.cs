74a292570e6dc2f9e61e7e76e2ee06f26d828a47
jose luis <jotaele89@gmail.com>
2017-11-22 21:48:04 -0500

    jose luis

---
+++ AlumnosWebApp/Models/Tarea.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlumnosWebApp.Models
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string ArchivoURL { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public DateTime FechaLimite { get; set; }
+    }
+}
